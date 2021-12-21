20b89549fb0cd6870077ee59124e22947a860fe9
Krystian <Krystian.Dutkait@gmail.com>
2021-12-21 10:47:42 +0100

    Add post.php with class Post

---
+++ core/post.php
@@ -0,0 +1,47 @@
+<?php 
+
+class Post{
+    //db stuff
+    private $conn;
+    private $table = 'posts';
+
+    //post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $create_at;
+
+    //constructor with db connection
+    public function __construct($db){
+        $this -> conn = $db;
+
+    }
+    //getting posts from our datebase
+        public function read(){
+                //create query
+                $query = 'SELECT
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.create_at
+                FROM
+                ' .$this -> table . ' p 
+                LEFT JOIN
+                    categories c ON p.categotry_id = c.id
+                    ORDERED BY p.created DESC';
+                //preapere statement
+                $stmt = $this -> conn -> prepare($query);
+                //execute query
+                $stmt -> execute();
+
+                return $stmt;
+            }
+}
+
+?>
